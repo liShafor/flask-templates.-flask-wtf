064bad3017bf5ae1424b636be9614f97a27e8a8e
liShafor <elizavetashaforostovaxr@gmail.com>
2025-04-04 22:33:52 +0300

    +Автоматический ответ

---
+++ automatic-response/main.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/<name>')
+@app.route('/index/<name>')
+def index(name):
+    return render_template('index.html', title=name)
+
+
+@app.route("/training/<prof>")
+def training(prof):
+    engineer = any(map(lambda x: x in prof,
+                       ("инженер", "строитель")))
+    return render_template("prof.html", engineer=engineer)
+
+
+@app.route("/list_prof/<listt>")
+def list_prof(listt):
+    lst = [
+        "инженер-исследователь",
+        "пилот",
+        "строитель",
+        "экзобиолог",
+        "врач",
+        "инженер по терраформированию",
+        "климатолог",
+        "специалист по радиационной защите",
+        "астрогеолог", "гляциолог",
+        "инженер жизнеобеспечения",
+        "метеоролог",
+        "оператор марсохода",
+        "киберинженер",
+        "штурман",
+        "пилот дронов"
+    ]
+    return render_template("list_prof.html", list_type=listt, jobs=lst)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    data =\
+        {
+            'title': 'answer',
+            'surname': 'Watny',
+            'name': 'Mark',
+            'education': 'Выше среднего',
+            'profession': 'Штурман марсохода',
+            'sex': 'male',
+            'motivation': 'Всегда мечтал застрять на Марсе!',
+            'ready': True
+        }
+    return render_template("auto_answer.html", **data)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
